e6e05434bf9e06e4af988a724d3db5da7f04def4
sfspacov <stephan_fs@hotmail.com>
2023-03-13 19:16:57 -0500

    Post and Put requests

--- src/contacts/contacts.controller.ts
+++ src/contacts/contacts.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, NotFoundException, Param, Post, Put } from '@nestjs/common';
 
 @Controller('contacts')
 export class ContactsController {
@@ -15,12 +15,44 @@ export class ContactsController {
     }
 
     @Get('/:id')
-    getOne(@Param("id") id: number) {
+    getOne(@Param("id") id) {
         let c = this.contacts.find(x => x.id == id)
 
         if (!c)
             throw new NotFoundException();
 
         return {...c};
+    }
+
+    @HttpCode(HttpStatus.CREATED)
+    @Post()
+    createContacts(@Body() body){
+        let ids = this.contacts.map(x => x.id);
+        let newId = 1 + Math.max(...ids);
+
+        if (body instanceof Array) {
+            let contacts = body;
+            contacts.forEach((x, i) => x.id = newId + 1);
+            this.contacts.push(...contacts);
+            return contacts;
+        } else {
+            let contact = body;
+            contact.id = newId;
+            this.contacts.push(contact);
+            return contact;
+        }
+    }
+
+    @Put('/:id')
+    updateContact(@Param("id") id, @Body() contact){
+        
+        let index = this.contacts.findIndex(c => c.id == id);
+
+        if (index===-1)
+            throw new NotFoundException();            
+
+        contact.id = parseInt(id);
+        this.contacts[index] = contact;
+        return contact;
     }
 }
