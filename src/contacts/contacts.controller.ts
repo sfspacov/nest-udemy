3f7d0830b17a60fee9294b40fc1e88769d4b32e9
sfspacov <stephan_fs@hotmail.com>
2023-03-14 15:42:14 -0500

    implement services

--- src/contacts/contacts.controller.ts
+++ src/contacts/contacts.controller.ts
@@ -1,81 +1,64 @@
 import { Body, Controller, Delete, Get, HttpCode, HttpStatus, NotFoundException, Param, Patch, Post, Put } from '@nestjs/common';
+import { ContactsService } from './contacts.service';
 
 @Controller('contacts')
 export class ContactsController {
-    contacts = [
-        { id: 1, name: 'Vinod', email: 'vinod@gmail.com' },
-        { id: 2, name: 'John', email: 'john@gmail.com' },
-        { id: 3, name: 'Slash', email: 'slash@gmail.com' },
-        { id: 4, name: 'Elton', email: 'elton@gmail.com' },
-    ];
+    constructor(private service: ContactsService) {
 
-    @Get()
-    getAll() {
-        return [...this.contacts];
-    }
-
-    @Get('/:id')
-    getOne(@Param("id") id) {
-        let c = this.contacts.find(x => x.id == id)
-
-        if (!c)
-            throw new NotFoundException();
-
-        return { ...c };
     }
 
     @HttpCode(HttpStatus.CREATED)
     @Post()
-    create(@Body() body) {
-        let ids = this.contacts.map(x => x.id);
-        let newId = 1 + Math.max(...ids);
-
+    addOneContact(@Body() body) {
         if (body instanceof Array) {
-            let contacts = body;
-            contacts.forEach((x, i) => x.id = newId + 1);
-            this.contacts.push(...contacts);
-            return contacts;
+            return this.service.addManyContacts(body);
         } else {
-            let contact = body;
-            contact.id = newId;
-            this.contacts.push(contact);
-            return contact;
+            return this.service.addOneContact(body);
         }
+    }
+
+    @Get()
+    getAll() {
+        return this.service.getAll();
+    }
+
+    @Get('/:id')
+    getById(@Param("id") id) {
+        if (this.service.exists(id)) {
+            return this.service.getById(id);
+        }
+        throw new NotFoundException();
     }
 
     @HttpCode(HttpStatus.NO_CONTENT)
     @Put('/:id')
     update(@Param("id") id, @Body() contact) {
-
-        let index = this.contacts.findIndex(c => c.id == id);
-
-        if (index === -1)
-            throw new NotFoundException();
-
-        contact.id = parseInt(id);
-        this.contacts[index] = contact;
+        if (this.service.exists(id)) {
+            this.service.replace(id, contact);
+            return;
+        }
+        throw new NotFoundException();
     }
 
     @HttpCode(HttpStatus.NO_CONTENT)
     @Patch('/:id')
     partialUpdate(@Param("id") id, @Body() contact) {
+        if (this.service.exists(id)) {
+            this.service.update(id, contact);
+            return;
+        }
+        throw new NotFoundException();
 
-        let index = this.contacts.findIndex(c => c.id == id);
-
-        if (index === -1)
-            throw new NotFoundException();
-
-        this.contacts[index] = { ...this.contacts[index], ...contact }
     }
 
     @HttpCode(HttpStatus.NO_CONTENT)
     @Delete('/:id')
     deleteContact(@Param('id') id) {
-        let index = this.contacts.findIndex(c => c.id == id);
-
-        if (index === -1)
-            throw new NotFoundException();
-
-        this.contacts.splice(index, 1);
+        if (this.service.exists(id)) {
+            this.service.delete(id);
+            return;
+        }
+        throw new NotFoundException();
+        
     }
 }
--- src/contacts/contacts.service.ts
+++ src/contacts/contacts.service.ts
@@ -1,4 +1,70 @@
 import { Injectable } from '@nestjs/common';
+import * as fs from 'fs'
 
+const filename = './contacts.json'
 @Injectable()
-export class ContactsService {}
+export class ContactsService {
+    contacts = [];
+    constructor() {
+        try {
+            let content = fs.readFileSync(filename, 'utf-8')
+            this.contacts = JSON.parse(content);
+        } catch (error) {
+            this.contacts = [];
+        }
+    }
+
+    replace(id, contact) {
+        let index = this.contacts.findIndex(c => c.id == id);
+        contact.id = parseInt(id);
+        this.contacts[index] = contact;
+        this.writeToFile();
+    }
+
+    update(id, contact) {
+        let index = this.contacts.findIndex(c => c.id == id);
+        this.contacts[index] = { ...this.contacts[index], ...contact }
+        this.writeToFile();
+    }
+
+    delete(id) {
+        let index = this.contacts.findIndex(c => c.id == id);
+
+        this.contacts.splice(index, 1);
+        this.writeToFile();
+    }
+    getAll() {
+        return [...this.contacts];
+    }
+
+    getById(id) {
+        return this.contacts.find(x => x.id == id);
+    }
+
+    writeToFile() {
+        fs.writeFileSync(filename, JSON.stringify(this.contacts), 'utf-8')
+    }
+
+    exists = (id) => this.contacts.findIndex(c => c.id == id) != -1;
+
+    get nextId() {
+        if (this.contacts.length === 0) return 1;
+        let ids = this.contacts.map(c => c.id);
+        return 1 + Math.max(...ids);
+    }
+
+    addOneContact(contact) {
+        contact.id = this.nextId;
+        this.contacts.push(contact);
+        this.writeToFile();
+        return contact;
+    }
+
+    addManyContacts(contacts) {
+        let nextId = this.nextId;
+        contacts.forEach((c, i) => c.id = i + nextId);
+        this.contacts.push(...contacts);
+        this.writeToFile();
+        return contacts;
+    }
+}
